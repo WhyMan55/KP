9529960718d7eba40862954218d3c84e1b128991
Антон Ушаков <bruuuhv123456@gmail.com>
2024-12-10 08:45:31 +0300

    Update noway.c

--- noway.c
+++ noway.c
@@ -9,92 +9,20 @@
 #define FILENAME "output.csv"
 #define MAX_T 100
 
-unsigned long long factorial(int n) {
-    if (n == 0 || n == 1) return 1;
-    unsigned long long result = 1;
-    for (int i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
-
-
-double* values;
-double F1(double x) {
-    return log(fabs(sin(x)) + pow(x, 2.0 / 5.0)) / log(5);
-}
-double F2(double x) {
-    if (x <= 1) {
-        return (pow(x, 2) - 7 * x + 3) / exp(x);
-    }
-    else {
-        return log(pow(x, 2));
-    }
-}
-double F3(double x) {
-    double sum = 0.0;
-    for (int n = 0; n < MAX_T; n++) {
-        double term = ((double)factorial(2 * n) / ((pow(2, 2 * n) * pow(factorial(n), 2)))) * (pow(x, -2 * n) / (2 * n));
-        sum += term;
-        if (fabs(term) < precision) {
-            break;
-        }
-    }
-    return sum;
-}
+unsigned long long factorial(int n);
 
 int count = 0;
+double* values;
+double F1(double);
+double F2(double);
+double F3(double);
 
-int input_file() {
-    FILE* file = fopen("input.csv", "r");
-    if (file == NULL) {
-        return -1;
-    }
-    values = (double*)malloc(sizeof(file));
-    count = 0;
-    while (fscanf(file, "%lf", &values[count]) != EOF) {
-        count++;
-    }
-    fclose(file);
-}
-int output_to_file(double values[], int count) {
-    FILE* file = fopen(FILENAME, "w");
-    if (file == NULL) {
-        return -1;
-    }
-    for (int i = 0; i < count; i++) {
-        fprintf(file, "%d, %lf\n", i + 1, values[i]);
-    }
-    fclose(file);
-    printf("Данные записаны в файл %s\n", FILENAME);
-}
-double find_max(double values[], int count) {
-    double max;
-    max = values[0];
-    for (int i = 1; i < count; i++) {
-        if (values[i] > max) max = values[i];
-    }
-    return max;
-}
-double find_min(double values[], int count) {
-    double min;
-    min = values[0];
-    for (int i = 1; i < count; i++) {
-        if (values[i] < min) min = values[i];
-    }
-    return min;
-}
-int linear_search(double values[], int count, double target) {
-    for (int i = 0; i < count; i++) {
-        if (fabs(values[i] - target) < precision) {
-            return i;
-        }
-    }
-    return -1;
-}
-double differentiate(double (*func)(double), double x, double h) {
-    return (func(x + h) - func(x)) / h;
-}
+int input_file();
+int output_to_file(double values[], int);
+double find_max(double values[], int);
+double find_min(double values[], int);
+int linear_search(double values[], int, double);
+double differentiate(double (*func)(double), double, double);
 
 int main() {
     setlocale(LC_CTYPE, "RUS");
@@ -195,7 +123,7 @@ int main() {
                 count = 0;
                 while (count < n) {
                     double x = xmin + 1.f * (xmax - xmin) * rand() / RAND_MAX;
-                    values[count++] = F3(x);
+                    values[count++] = F1(x);
 
                 }
                 break;
@@ -218,3 +146,90 @@ int main() {
 
         return 0;
     }
+
+double differentiate(double (*func)(double), double x, double h) {
+    return (func(x + h) - func(x)) / h;
+}
+
+    int linear_search(double values[], int count, double target) {
+        for (int i = 0; i < count; i++) {
+            if (fabs(values[i] - target) < precision) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+        int input_file() {
+            FILE* file = fopen("input.csv", "r");
+            if (file == NULL) {
+                return -1;
+            }
+            values = (double*)malloc(sizeof(file));
+            count = 0;
+            while (fscanf(file, "%lf", &values[count]) != EOF) {
+                count++;
+            }
+            fclose(file);
+        }
+
+        int output_to_file(double values[], int count) {
+            FILE* file = fopen(FILENAME, "w");
+            if (file == NULL) {
+                return -1;
+            }
+            for (int i = 0; i < count; i++) {
+                fprintf(file, "%d, %lf\n", i + 1, values[i]);
+            }
+            fclose(file);
+            printf("Данные записаны в файл %s\n", FILENAME);
+        }
+
+        double find_max(double values[], int count) {
+            double max;
+            max = values[0];
+            for (int i = 1; i < count; i++) {
+                if (values[i] > max) max = values[i];
+            }
+            return max;
+        }
+        double find_min(double values[], int count) {
+            double min;
+            min = values[0];
+            for (int i = 1; i < count; i++) {
+                if (values[i] < min) min = values[i];
+            }
+            return min;
+        }
+
+        unsigned long long factorial(int n) {
+            if (n == 0 || n == 1) return 1;
+            unsigned long long result = 1;
+            for (int i = 2; i <= n; i++) {
+                result *= i;
+            }
+            return result;
+        }
+
+        double F1(double x) {
+            return log(fabs(sin(x)) + pow(x, 2.0 / 5.0)) / log(5);
+        }
+        double F2(double x) {
+            if (x <= 1) {
+                return (pow(x, 2) - 7 * x + 3) / exp(x);
+            }
+            else {
+                return log(pow(x, 2));
+            }
+        }
+        double F3(double x) {
+            double sum = 0.0;
+            for (int n = 0; n < MAX_T; n++) {
+                double term = ((double)factorial(2 * n) / ((pow(2, 2 * n) * pow(factorial(n), 2)))) * (pow(x, -2 * n) / (2 * n));
+                sum += term;
+                if (fabs(term) < precision) {
+                    break;
+                }
+            }
+            return sum;
+        }
